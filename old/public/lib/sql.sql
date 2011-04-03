04f9c24780df8f0e2de50f192c5483ff595f50f9
Arunjit Singh <arunjitsingh1190@gmail.com>
2011-04-04 01:43:38 +0530

    ignoring some files

--- old/public/lib/sql.sql
+++
@@ -1,4 +0,0 @@
-SELECT groups.id, groups.group, groups.name
-FROM groups INNER JOIN UserGroups
-    ON groups.id = UserGroups.group_id
-WHERE UserGroups.user_id = 1;
--- old/testwalk.js
+++
@@ -1,5 +0,0 @@
-var walk = require('./asyncwalk').walk,
-    sys  = require('util');
-walk('./',
-    function (file, next) { sys.log(file); next(); },
-    function (err) { sys.log("done. error: " + err); });
