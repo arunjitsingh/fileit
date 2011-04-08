bb47a2472ca9f95029a74b6efb4b0d749478f586
Arunjit Singh <arunjitsingh1190@gmail.com>
2011-04-08 11:59:37 +0530

    version 0.1.0ß

--- test/file.run.js
+++
@@ -1,7 +0,0 @@
-var log = require('logging');
-var FI = require('../file/info');
-var fi = new FI({root:'/Volumes/OLFS/root'});
-fi.info('users/arunjitsingh', function(err, info) {
-    if (err) log(err);
-    else log(info);
-});
